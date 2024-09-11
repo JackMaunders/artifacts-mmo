a5a9bf9cce5827d4e5fa0f3ae7461612cdedce30
Jack Maunders <jack.maunders@sky.uk>
2024-09-11 17:25:54 +0100

    Basic server

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"jackmaunders/artifacts-mmo/actions"
+	"net/http"
 )
 
-func main() {
+func handleMove(w http.ResponseWriter, r *http.Request) {
 	jack := actions.Character{Name: "Jack"}
 	jack.Move(actions.MoveCorrdinates{X: 5, Y: 4})
+}
+
+func main() {
+	http.HandleFunc("/move", handleMove)
+
+	err := http.ListenAndServe(":3001", nil)
 }
